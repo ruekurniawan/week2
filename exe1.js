2d044e4129cca33b1fb185d48183e6479ddcab34
ruekurniawan <ruekurniawan>
2018-10-22 19:06:30 +0700

    masukkan data

---
+++ exe1.js
@@ -0,0 +1,76 @@
+/* 
+SOAL NO 1 
+
+ALGORITMA
+1. Masukkan variabel "gaya"
+2. Masukkan variabel "massa" = 600
+3. Masukkan variabel "percepatan" = 2
+4. Hitung perkalian "masa" dan "percepatan"
+5. Hasil perhitungan dimasukkan ke variabel "gaya"
+6. Tampilkan nilai "gaya"
+
+PSEUCODE
+STORE "gaya" without any value
+STORE "massa" with any value
+STORE "percepatan" with any value
+
+CALCULATE "gaya" times "massa"
+SET "gaya" value with calculation result
+
+DISPLAY "gaya"
+*/
+
+/* 
+SOAL NO 2
+
+ALGORITMA
+1. Masukkan variabel "tahun"
+2. Jika "tahun" habis dibagi 4 dan habis di bagi 100 dan habis di bagi 400 lanjutkan langkah 3, jika "tahun" habis di bagi 4 dan tidak habis dibagi 100 lanjutkan langkah 3, jika tidak lanjutkan lanjutkan langkah 4
+3. Tampilkan nila "INI KABISAT"
+4. Tampilkan nila "INI BUKAN KABISAT"
+
+PSEUCODE
+STORE "tahun" with any value
+
+IF"tahun" MOD 4 equals 0 AND "tahun" MOD 100 not equals 0
+	DO "Ini kabisat"
+ELSE IF "tahun" MOD 4 equals 0 AND "tahun" MOD 100 equals 0 AND "tahun" MOD 400 equals 0
+	DO "Ini kabisat"
+ELSE
+	DO "Ini bukan kabisat"
+*/
+
+
+/*
+SOAL NO 3
+
+ALGORITMA
+1. Masukan variabel "baju"
+2. Hitung "baju" yang dimasukan sampai 20, bila sudah sampai 20 lanjutkan langkah 3
+3. Total "baju" = 20 mesin bisa dinyalakan
+
+PSEUCODE 
+STORE "baju" with 0
+WHILE "baju" < 20
+	ADD "baju" by 1
+DISPLAY "Baju sudah masuk dan mesin siap di jalankan"
+*/
+
+/*
+SOAL NO 4
+
+ALGORITMA
+1. Masukkan variabel "anak"
+2. Hitung "anak" yang diperiksa sampai 40 jika anak memiliki kuku panjang lanjutkan langkah 3, jika anak tidak memiliki kuku panjang maka lanjutkan langkah 4
+3. Jika anak memiliki kuku panjang maka guru akan menghukum anak
+4. Jika anak tidak memilik kuku panjang maka guru akan memuji anak
+
+PSEUCODE
+STORE "anak" with 0
+WHILE "anak" < 40
+	IF "anak" equals memiliki kuku panjang
+		DISPLAY "Guru menghukum siswa"
+	ELSE
+		DISPLAY "Guru memuji siswa"
+	ADD "anak" by 1
+*/
