b3fb51bc0be7b4863be3d0f390e54fc9dfdb2f8a
ruekurniawan <ruekurniawan>
2018-10-26 00:35:24 +0700

    Penambahan data Kamis

---
+++ Kamis/exe9.2.js
@@ -0,0 +1,9 @@
+var num1 = 5;
+var num2 = 6;
+function calculateMultiply(num1,num2){
+	hasilPerkalian=num1*num2;
+	return hasilPerkalian;
+
+}
+var hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian);
